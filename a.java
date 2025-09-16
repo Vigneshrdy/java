c91a64169657e2b62333c353e7dbef4855f7ad21
Vigneshrdy <vigneshreddy693@gmail.com>
2025-09-16 11:44:09 +0530

    Java

---
+++ a.java
@@ -0,0 +1,13 @@
+package kmit;
+public class A
+{
+    public int i;
+    public A()
+    {
+        System.out.println("Constructor called");
+    }
+    void m1()
+    {
+        System.out.println("M1 invoked");
+    }
+}   
